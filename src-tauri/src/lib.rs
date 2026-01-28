a4c51708863d4d4158aec3ce6b599547cf43464f
Poseidoncode <posidomhu@gmail.com>
2026-01-29 01:23:27 +0800

    2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,5 +9,12 @@ use models::{BranchOptions, CloneOptions, CommitOptions, Settings, StashOptions}
 use std::sync::Mutex;
 use tauri::{Manager, State};
 
-// 其餘內容維持原樣
-// ...
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
